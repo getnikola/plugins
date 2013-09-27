3c211fbcf9e02dcb26a09a8026ea91c1daad3be9
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-09-27 16:51:03 +0200

    have flake8 shut up
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- scripts/build_plugins.py
+++ scripts/build_plugins.py
@@ -3,23 +3,24 @@
 """Make sanity checks on the provided plugins."""
 
 from __future__ import unicode_literals, print_function
-import codecs
 from contextlib import contextmanager
 import glob
 import json
 import os
-import shutil
 import subprocess
 import colorama
 from progressbar import ProgressBar
 
 BASE_URL = "http://plugins.nikola.ralsina.com.ar/v6/"
 
+
 def error(msg):
     print(colorama.Fore.RED + "ERROR:" + msg)
 
+
 def plugin_list():
     return [plugin.split('/')[-1] for plugin in glob.glob("plugins/*")]
+
 
 def build_plugin(plugin=None):
     if plugin is None:  # Check them all
@@ -34,14 +35,17 @@ def build_plugin(plugin=None):
 
     if os.path.isdir('plugins/'+plugin):
         with cd('plugins/'):
-            subprocess.check_call('zip -r ../output/v6/{0}.zip {0}'.format(plugin), stdout=subprocess.PIPE, shell=True)
+            subprocess.check_call('zip -r ../output/v6/{0}.zip '
+                                  '{0}'.format(plugin), stdout=subprocess.PIPE,
+                                  shell=True)
 
     plugins_dict = {}
     for plugin in glob.glob('plugins/*/'):
         t_name = os.path.basename(plugin[:-1])
         plugins_dict[t_name] = BASE_URL + t_name + ".zip"
     with open(os.path.join("output", "v6", "plugins.json"), "wb+") as outf:
-        json.dump(plugins_dict, outf, indent=4, ensure_ascii=True, sort_keys=True)
+        json.dump(plugins_dict, outf, indent=4, ensure_ascii=True,
+                  sort_keys=True)
 
 
 @contextmanager
