7b8b2af1d78d120b4008fd7867349423d901373c
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-09-27 14:55:56 -0300

    graphviz update

--- output/v6/plugin_data.js
+++ output/v6/plugin_data.js
@@ -1,4 +1,13 @@
 var data = {
+    "graphviz": {
+        "author": "Roberto Alsina", 
+        "description": "Graph directives based on Graphviz, compatible with Sphinx", 
+        "maxver": null, 
+        "minver": null, 
+        "name": "graphviz", 
+        "readme": "No README.md file available.", 
+        "version": "0.1"
+    }, 
     "tags": {
         "author": "Puneeth Chaganti", 
         "description": "Manage the tags for your site.", 
