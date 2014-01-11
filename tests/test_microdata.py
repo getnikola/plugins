3f7b2a6791089e92a8021d415a7e7d36c8b92ecb
Ivan Teoh <ivan.teoh@gmail.com>
2014-01-11 11:56:51 +1100

    Add microdata plugin.

---
+++ tests/test_microdata.py
@@ -0,0 +1,343 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+# This code is so you can run the samples without installing the package,
+# and should be before any import touching nikola, in any file under tests/
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+import unittest
+
+from nikola.utils import LOGGER
+import logbook
+from test_rst_compiler import ReSTExtensionTestCase
+
+
+class ItemPropTestCase(ReSTExtensionTestCase):
+
+    @staticmethod
+    def setUpClass():
+        LOGGER.notice('--- TESTS FOR ItemProp')
+        LOGGER.level = logbook.WARNING
+
+    @staticmethod
+    def tearDownClass():
+        sys.stdout.write('\n')
+        LOGGER.level = logbook.NOTICE
+        LOGGER.notice('--- END OF TESTS FOR ItemProp')
+
+    def test_itemprop(self):
+        # the result should be
+        # <p><span itemprop="name">Test</span></p>
+        self.sample = ':itemprop:`Test <name>`'
+        self.basic_test()
+        self.assertHTMLContains("span", attributes={"itemprop": "name"},
+                                text="Test")
+        self.assertHTMLContains("p")
+
+    def test_itemprop_image(self):
+        # the result should be
+        # <img itemprop="photo" src="apple-pie.jpg" />
+        self.sample = ":itemprop:`<photo|apple-pie.jpg|img>`"
+        self.basic_test()
+        self.assertHTMLContains("img", attributes={"itemprop": "photo", "src": "apple-pie.jpg"},
+                                text="")
+        self.assertHTMLContains("p")
+
+    def test_itemprop_time(self):
+        # the result should be
+        # <time datetime="PT30M" itemprop="prepTime">30 min</time>
+        self.sample = ":itemprop:`30 min <prepTime|PT30M|time>`"
+        self.basic_test()
+        self.assertHTMLContains("time", attributes={"itemprop": "prepTime", "datetime": "PT30M"},
+                                text="30 min")
+        self.assertHTMLContains("p")
+
+    def test_itemprop_meta(self):
+        # the result should be
+        # <meta itemprop="datePublished" content="2009-05-08">May 8, 2009
+        expected = (
+            '<p>'
+            '<meta content="2009-05-08" itemprop="datePublished" /> May 8, 2009'
+            '</p>\n'
+        )
+        self.sample = ":itemprop:`<datePublished|2009-05-08|meta>` May 8, 2009"
+        self.basic_test()
+        self.assertHTMLContains("meta", attributes={"itemprop": "datePublished", "content": "2009-05-08"},
+                                text="")
+        self.assertHTMLContains("p")
+        self.assertHTMLEqual(expected.strip())
+
+
+class ItemPropUrlTestCase(ReSTExtensionTestCase):
+
+    @staticmethod
+    def setUpClass():
+        LOGGER.notice('--- TESTS FOR ItemPropUrl')
+        LOGGER.level = logbook.WARNING
+
+    @staticmethod
+    def tearDownClass():
+        sys.stdout.write('\n')
+        LOGGER.level = logbook.NOTICE
+        LOGGER.notice('--- END OF TESTS FOR ItemPropUrl')
+
+    def test_itemprop_url(self):
+        # the result should be
+        # <p><a href="http://somewhere/" itemprop="url">Test</a></p>
+        self.sample = ':itemprop:`Test <url:http://somewhere/>`'
+        self.basic_test()
+        self.assertHTMLContains("a", attributes={"itemprop": "url", "href": "http://somewhere/"},
+                                text="Test")
+        self.assertHTMLContains("p")
+
+
+class ItemScopeTestCase(ReSTExtensionTestCase):
+
+    @staticmethod
+    def setUpClass():
+        LOGGER.notice('--- TESTS FOR ItemScope')
+        LOGGER.level = logbook.WARNING
+
+    @staticmethod
+    def tearDownClass():
+        sys.stdout.write('\n')
+        LOGGER.level = logbook.NOTICE
+        LOGGER.notice('--- END OF TESTS FOR ItemScope')
+
+    def test_itemscope(self):
+        # the result should be
+        # <div itemscope itemtype="http://data-vocabulary.org/Person">
+        # My name is John Doe
+        # </div>
+        self.sample = """.. itemscope:: Person
+
+            My name is John Doe
+        """
+        self.basic_test()
+        self.assertHTMLContains("div", attributes={"itemscope": "", "itemtype": "http://data-vocabulary.org/Person"},
+                                text="My name is John Doe")
+
+
+class ItemScopePropTestCase(ReSTExtensionTestCase):
+
+    @staticmethod
+    def setUpClass():
+        LOGGER.notice('--- TESTS FOR ItemScopeProp')
+        LOGGER.level = logbook.WARNING
+
+    @staticmethod
+    def tearDownClass():
+        sys.stdout.write('\n')
+        LOGGER.level = logbook.NOTICE
+        LOGGER.notice('--- END OF TESTS FOR ItemScopeProp')
+
+    def test_itemscope_itemprop(self):
+        # the result should be
+        # <div itemscope itemtype="http://data-vocabulary.org/Person">
+        # My name is <span itemprop="name">John Doe</span>
+        # </div>
+        self.sample = """.. itemscope:: Person
+
+            My name is :itemprop:`John Doe <name>`
+        """
+        self.basic_test()
+        self.assertHTMLContains("div", attributes={"itemscope": "", "itemtype": "http://data-vocabulary.org/Person"},
+                                text="My name is ")
+        self.assertHTMLContains("span", attributes={"itemprop": "name"},
+                                text="John Doe")
+
+
+class ItemScopeTagTestCase(ReSTExtensionTestCase):
+
+    @staticmethod
+    def setUpClass():
+        LOGGER.notice('--- TESTS FOR ItemScopeTag')
+        LOGGER.level = logbook.WARNING
+
+    @staticmethod
+    def tearDownClass():
+        sys.stdout.write('\n')
+        LOGGER.level = logbook.NOTICE
+        LOGGER.notice('--- END OF TESTS FOR ItemScopeTag')
+
+    def test_itemscope_tag(self):
+        # the result should be
+        # <p itemscope itemtype="http://data-vocabulary.org/Person">
+        # My name is <span itemprop="name">John Doe</span>
+        # </p>
+        self.sample = """.. itemscope:: Person
+            :tag: p
+
+            My name is :itemprop:`John Doe <name>`
+        """
+        self.basic_test()
+        self.assertHTMLContains("p", attributes={"itemscope": "", "itemtype": "http://data-vocabulary.org/Person"},
+                                text="My name is ")
+        self.assertHTMLContains("span", attributes={"itemprop": "name"},
+                                text="John Doe")
+
+    def test_itemscope_tag_span(self):
+        # the result should be
+        # <span itemprop="ingredient" itemscope itemtype="http://data-vocabulary.org/RecipeIngredient">
+        #  Thinly-sliced <span itemprop="name">apples</span>:<span itemprop="amount">6 cups</span>
+        # </span>
+        expected = (
+            '<span itemprop="ingredient" itemscope itemtype="http://data-vocabulary.org/RecipeIngredient">'
+            'Thinly-sliced <span itemprop="name">apples</span>:'
+            '<span itemprop="amount">6 cups</span>'
+            '</span>'
+        )
+        self.sample = """.. itemscope:: RecipeIngredient
+            :tag: span
+            :itemprop: ingredient
+
+            Thinly-sliced :itemprop:`apples <name>`::itemprop:`6 cups <amount>`
+        """
+        self.basic_test()
+        self.assertHTMLEqual(expected.strip())
+
+    def test_itemscope_h1(self):
+        # the result should be
+        # <div itemscope itemtype="http://data-vocabulary.org/Recipe">
+        # <h1 itemprop="name">Grandma\'s Holiday Apple Pie</h1>
+        # </div>
+        self.sample = """.. itemscope:: Recipe
+        
+            :itemprop:`Grandma's Holiday Apple Pie <name||h1>`
+        """
+        self.basic_test()
+        self.assertHTMLContains("div", attributes={"itemscope": "", "itemtype": "http://data-vocabulary.org/Recipe"},
+                                text="")
+        self.assertHTMLContains("h1", attributes={"itemprop": "name"},
+                                text="Grandma's Holiday Apple Pie")
+
+    def test_itemscope_div(self):
+        # the result should be
+        # <div itemscope itemtype="http://data-vocabulary.org/Recipe">
+        # <div itemprop="instructions">
+        # <p itemprop="instruction">Cut and peel apples.</p>
+        # <p itemprop="instruction">Mix sugar and cinnamon. Use additional sugar for tart apples.</p>
+        # </div></div>
+        expected = (
+            '<div itemscope itemtype="http://data-vocabulary.org/Recipe">'
+            '<div itemprop="instructions">'
+            '<p itemprop="instruction">Cut and peel apples.</p>'
+            '<p itemprop="instruction">Mix sugar and cinnamon. Use additional sugar for tart apples.</p>'
+            '</div></div>'
+        )
+        self.sample = """.. itemscope:: Recipe
+        
+            .. itempropblock:: instructions
+
+                .. itempropblock:: instruction
+                    :tag: p
+
+                    Cut and peel apples. 
+
+                .. itempropblock:: instruction
+                    :tag: p
+
+                    Mix sugar and cinnamon. Use additional sugar for tart apples.
+        """
+        self.basic_test()
+        self.assertHTMLEqual(expected.strip())
+
+
+class ItemScopeNestedTestCase(ReSTExtensionTestCase):
+
+    @staticmethod
+    def setUpClass():
+        LOGGER.notice('--- TESTS FOR ItemScopeNested')
+        LOGGER.level = logbook.WARNING
+
+    @staticmethod
+    def tearDownClass():
+        sys.stdout.write('\n')
+        LOGGER.level = logbook.NOTICE
+        LOGGER.notice('--- END OF TESTS FOR ItemScopeNested')
+
+    def test_nested_scope(self):
+        # the result should be
+        # <div itemscope itemtype="http://data-vocabulary.org/Person">
+        # <p>My name is <span itemprop="name">John Doe</span></p>
+        # <p itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address">
+        # My name is <span itemprop="name">John Doe</span>
+        # </p></div>
+        expected = (
+            '<div itemscope itemtype="http://data-vocabulary.org/Person">'
+            '<p>'
+            'My name is <span itemprop="name">John Doe</span>'
+            '</p>'
+            '<p itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address">'
+            'My name is <span itemprop="name">John Doe</span>'
+            '</p>'
+            '</div>'
+        )
+        self.sample = """.. itemscope:: Person
+
+            My name is :itemprop:`John Doe <name>`
+
+            .. itemscope:: Address
+                :tag: p
+                :itemprop: address
+
+                My name is :itemprop:`John Doe <name>`
+        """
+        self.basic_test()
+        self.assertHTMLContains("div", attributes={"itemscope": "",
+                                "itemtype": "http://data-vocabulary.org/Person"},
+                                text="")
+        self.assertHTMLEqual(expected.strip())
+
+
+class ItemScopeNestedCompactTestCase(ReSTExtensionTestCase):
+
+    @staticmethod
+    def setUpClass():
+        LOGGER.notice('--- TESTS FOR ItemScopeNestedCompact')
+        LOGGER.level = logbook.WARNING
+
+    @staticmethod
+    def tearDownClass():
+        sys.stdout.write('\n')
+        LOGGER.level = logbook.NOTICE
+        LOGGER.notice('--- END OF TESTS FOR ItemScopeNestedCompact')
+
+    def test_nested_scope_compact(self):
+        # the result should be
+        # <p itemscope itemtype="http://data-vocabulary.org/Person">
+        # My name is <span itemprop="name">John Doe</span>
+        # <span itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address">
+        # My name is <span itemprop="name">John Doe</span>
+        # </span></p>
+        expected = (
+            '<p itemscope itemtype="http://data-vocabulary.org/Person">'
+            'My name is <span itemprop="name">John Doe</span>'
+            '<span itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address">'
+            'My name is <span itemprop="name">John Doe</span>'
+            '</span>'
+            '</p>'
+        )
+        self.sample = """.. itemscope:: Person
+            :tag: p
+            :compact:
+
+            My name is :itemprop:`John Doe <name>`
+
+            .. itemscope:: Address
+                :tag: span
+                :itemprop: address
+
+                My name is :itemprop:`John Doe <name>`
+        """
+        self.basic_test()
+        self.assertHTMLContains("p", attributes={"itemscope": "",
+                                "itemtype": "http://data-vocabulary.org/Person"},
+                                text="My name is ")
+        self.assertHTMLEqual(expected.strip())
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_rst_compiler.py
@@ -0,0 +1,197 @@
+# coding: utf8
+# Author: Rodrigo Bistolfi
+# Date: 03/2013
+
+
+""" Test cases for Nikola ReST extensions.
+A base class ReSTExtensionTestCase provides the tests basic behaivor.
+Subclasses must override the "sample" class attribute with the ReST markup.
+The sample will be rendered as HTML using publish_parts() by setUp().
+One method is provided for checking the resulting HTML:
+
+    * assertHTMLContains(element, attributes=None, text=None)
+
+The HTML is parsed with lxml for checking against the data you provide. The
+method takes an element argument, a string representing the *name* of an HTML
+tag, like "script" or "iframe". We will try to find this tag in the document
+and perform the tests on it. You can pass a dictionary to the attributes kwarg
+representing the name and the value of the tag attributes. The text kwarg takes
+a string argument, which will be tested against the contents of the HTML
+element.
+One last caveat: you need to url unquote your urls if you are going to test
+attributes like "src" or "link", since the HTML rendered by docutils will be
+always unquoted.
+
+"""
+
+
+from __future__ import unicode_literals, absolute_import
+
+# This code is so you can run the samples without installing the package,
+# and should be before any import touching nikola, in any file under tests/
+import os
+import sys
+extra_plugin_dir = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, extra_plugin_dir)
+
+
+import codecs
+try:
+    from io import StringIO
+except ImportError:
+    from StringIO import StringIO  # NOQA
+import tempfile
+
+from lxml import html
+import unittest
+from yapsy.PluginManager import PluginManager
+
+from nikola import utils
+import nikola.plugins.compile.rest
+import nikola.plugins.compile.rest.listing
+from nikola.utils import STDERR_HANDLER
+from nikola.plugin_categories import (
+    Command,
+    Task,
+    LateTask,
+    TemplateSystem,
+    PageCompiler,
+    TaskMultiplier,
+    RestExtension,
+)
+from test_base import BaseTestCase
+
+
+class FakePost(object):
+
+    def __init__(self, title, slug):
+        self._title = title
+        self._slug = slug
+        self._meta = {'slug': slug}
+
+    def title(self):
+        return self._title
+
+    def meta(self, key):
+        return self._meta[key]
+
+    def permalink(self):
+        return '/posts/' + self._slug
+
+
+class FakeSite(object):
+    def __init__(self):
+        self.template_system = self
+        self.config = {
+            'DISABLED_PLUGINS': [],
+            'EXTRA_PLUGINS': [],
+            'DEFAULT_LANG': 'en',
+            'EXTRA_PLUGINS_DIRS': [extra_plugin_dir],
+        }
+        self.EXTRA_PLUGINS = self.config['EXTRA_PLUGINS']
+        self.plugin_manager = PluginManager(categories_filter={
+            "Command": Command,
+            "Task": Task,
+            "LateTask": LateTask,
+            "TemplateSystem": TemplateSystem,
+            "PageCompiler": PageCompiler,
+            "TaskMultiplier": TaskMultiplier,
+            "RestExtension": RestExtension,
+        })
+        self.loghandlers = [STDERR_HANDLER]
+        self.plugin_manager.setPluginInfoExtension('plugin')
+        extra_plugins_dirs = self.config['EXTRA_PLUGINS_DIRS']
+        if sys.version_info[0] == 3:
+            places = [
+                os.path.join(os.path.dirname(utils.__file__), 'plugins'),
+            ] + [path for path in extra_plugins_dirs if path]
+        else:
+            places = [
+                os.path.join(os.path.dirname(utils.__file__), utils.sys_encode('plugins')),
+            ] + [utils.sys_encode(path) for path in extra_plugins_dirs if path]
+        self.plugin_manager.setPluginPlaces(places)
+        self.plugin_manager.collectPlugins()
+
+        self.timeline = [
+            FakePost(title='Fake post',
+                     slug='fake-post')
+        ]
+
+    def render_template(self, name, _, context):
+        return('<img src="IMG.jpg">')
+
+
+class ReSTExtensionTestCase(BaseTestCase):
+    """ Base class for testing ReST extensions """
+
+    sample = 'foo'
+    deps = None
+
+    def setUp(self):
+        self.compiler = nikola.plugins.compile.rest.CompileRest()
+        self.compiler.set_site(FakeSite())
+        return super(ReSTExtensionTestCase, self).setUp()
+
+    def basic_test(self):
+        """ Parse cls.sample into a HTML document tree """
+        self.setHtmlFromRst(self.sample)
+
+    def setHtmlFromRst(self, rst):
+        """ Create html output from rst string """
+        tmpdir = tempfile.mkdtemp()
+        inf = os.path.join(tmpdir, 'inf')
+        outf = os.path.join(tmpdir, 'outf')
+        depf = os.path.join(tmpdir, 'outf.dep')
+        with codecs.open(inf, 'wb+', 'utf8') as f:
+            f.write(rst)
+        self.html = self.compiler.compile_html(inf, outf)
+        with codecs.open(outf, 'r', 'utf8') as f:
+            self.html = f.read()
+        os.unlink(inf)
+        os.unlink(outf)
+        if os.path.isfile(depf):
+            with codecs.open(depf, 'r', 'utf8') as f:
+                self.assertEqual(self.deps, f.read())
+            os.unlink(depf)
+        else:
+            self.assertEqual(self.deps, None)
+        os.rmdir(tmpdir)
+        self.html_doc = html.parse(StringIO(self.html))
+
+    def assertHTMLContains(self, element, attributes=None, text=None):
+        """ Test if HTML document includes an element with the given
+        attributes and text content
+
+        """
+        try:
+            tag = next(self.html_doc.iter(element))
+        except StopIteration:
+            raise Exception("<{0}> not in {1}".format(element, self.html))
+        else:
+            if attributes:
+                arg_attrs = set(attributes.items())
+                tag_attrs = set(tag.items())
+                self.assertTrue(arg_attrs.issubset(tag_attrs))
+            if text:
+                self.assertIn(text, tag.text)
+
+    def assertHTMLEqual(self, html):
+        """ Test if HTML document is the same
+        """
+        html_string = self.html.strip().replace('\n', '')
+        self.assertEqual(html_string, html)
+
+
+class ReSTExtensionTestCaseTestCase(ReSTExtensionTestCase):
+    """ Simple test for our base class :) """
+
+    sample = '.. raw:: html\n\n   <iframe src="foo" height="bar">spam</iframe>'
+
+    def test_test(self):
+        self.basic_test()
+        self.assertHTMLContains("iframe", attributes={"src": "foo"},
+                                text="spam")
+        self.assertRaises(Exception, self.assertHTMLContains, "eggs", {})
+
+if __name__ == "__main__":
+    unittest.main()
