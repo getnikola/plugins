782d380eba0d254e59dd8fcc63930dd2b2fd49ff
Blake Winton <bwinton@latte.ca>
2015-01-26 14:53:39 -0500

    Add the subindexes plugin.

---
+++ v7/subindexes/README.md
@@ -0,0 +1,3 @@
+This plugin generates an index page for each subdirectory of the posts.
+
+It uses the standard index configuration options.
---
+++ v7/subindexes/subindexes.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from collections import defaultdict
+import os
+
+from nikola.plugin_categories import Task
+from nikola.utils import config_changed
+
+
+class SubIndexes(Task):
+    """Render the indexes for each subdirectory in the blog."""
+
+    name = "render_subindexes"
+
+    def gen_tasks(self):
+        self.site.scan_posts()
+        yield self.group_task()
+
+        kw = {
+            "output_folder": self.site.config['OUTPUT_FOLDER'],
+            "filters": self.site.config['FILTERS'],
+            "index_file": self.site.config['INDEX_FILE'],
+            "strip_indexes": self.site.config['STRIP_INDEXES'],
+            "index_teasers": self.site.config['INDEX_TEASERS'],
+        }
+        template_name = "index.tmpl"
+        for lang in self.site.config['TRANSLATIONS']:
+            groups = defaultdict(list)
+            for p in self.site.timeline:
+                if p.is_post:
+                    dirname = os.path.dirname(p.destination_path(lang))
+                    dirname = [part for part in os.path.split(dirname) if part]
+                    if dirname == ["."]:
+                        continue
+                    dirname.reverse()
+                    part = ''
+                    while len(dirname):
+                        part = os.path.join(part, dirname.pop())
+                        groups[part].append(p)
+
+            for dirname, post_list in groups.items():
+                context = {}
+                context["items"] = []
+                should_render = True
+                output_name = os.path.join(kw['output_folder'], dirname, kw['index_file'])
+                short_destination = os.path.join(dirname, kw['index_file'])
+                link = short_destination.replace('\\', '/')
+                index_len = len(kw['index_file'])
+                if kw['strip_indexes'] and link[-(1 + index_len):] == '/' + kw['index_file']:
+                    link = link[:-index_len]
+                context["permalink"] = link
+
+                for post in post_list:
+                    # If there is an index.html pending to be created from
+                    # a story, do not generate the STORY_INDEX
+                    if post.destination_path(lang) == short_destination:
+                        should_render = False
+                    else:
+                        context["items"].append((post.title(lang),
+                                                 post.permalink(lang)))
+
+                context['index_teasers'] = kw['index_teasers']
+
+                if should_render:
+                    task = self.site.generic_post_list_renderer(lang, post_list,
+                                                                output_name,
+                                                                template_name,
+                                                                kw['filters'],
+                                                                context)
+                    task_cfg = {1: task['uptodate'][0].config, 2: kw}
+                    task['uptodate'] = [config_changed(task_cfg)]
+                    task['basename'] = self.name
+                    yield task
