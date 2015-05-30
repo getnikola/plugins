5b02b0d09e1cc9cbde12d6569353a27ef745267f
Chris Warrick <kwpolska@gmail.com>
2015-05-30 15:30:00 +0200

    Deprecate webapp and use it as a Coil advert
    close #77
    
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- v7/webapp/webapp.py
+++ v7/webapp/webapp.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# Copyright © 2015 The Coil Contributors
 # Copyright © 2014 Roberto Alsina
 
 # Permission is hereby granted, free of charge, to any
@@ -25,27 +26,16 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from __future__ import print_function, unicode_literals
-import json
-import os
-import webbrowser
-
-import bottle as b
-import mako
-
 from nikola.plugin_categories import Command
 
-_site = None
+import webbrowser
 
 
-def init_site():
-    _site.scan_posts(really=True)
-
-
-class Webapp(Command):
+class WebAppCoilAdvertisement(Command):
 
     name = "webapp"
-    doc_usage = "[[-u] theme_name] | [[-u] -l]"
-    doc_purpose = "install theme into current site"
+    doc_usage = ""
+    doc_purpose = "deprecated, use Coil CMS instead"
     cmd_options = [
         {
             'name': 'browser',
@@ -65,112 +55,9 @@ class Webapp(Command):
     ]
 
     def _execute(self, options, args):
-        global _site
-        _site = self.site
-        init_site()
-        port = options and options.get('port')
+        print("Nikola WebApp is not available anymore. It has been replaced by Coil CMS.")
+        print("Coil CMS is a full-featured CMS, ready to be used everywhere from small single-user sites environments to big corporate blogs.")
+        print("The most basic setup does not require a database and can be done in 2 minutes.")
+        print("Coil setup guide: http://coil.readthedocs.org/admin/setup/")
         if options and options.get('browser'):
-            webbrowser.open('http://localhost:{0}'.format(port))
-        b.run(host='localhost', port=port)
-
-    @staticmethod
-    @b.route('/')
-    def index():
-        context = {}
-        context['site'] = _site
-        return render('index.tpl', context)
-
-    @staticmethod
-    @b.route('/edit/<path:path>', method='POST')
-    @b.route('/edit/<path:path>', method='GET')
-    def edit(path):
-        context = {'path': path}
-        context['site'] = _site
-        context['json'] = json
-        post = None
-        for p in _site.timeline:
-            if p.source_path == path:
-                post = p
-                break
-        if post is None:
-            b.abort(404, "No such post or page")
-        context['post'] = post
-        return render('edit_post.tpl', context)
-
-    @staticmethod
-    @b.route('/save/<path:path>', method='POST')
-    def save(path):
-        # FIXME insecure pending defnull/bottle#411
-        context = {'path': path}
-        context['site'] = _site
-        post = None
-        for p in _site.timeline:
-            if p.source_path == path:
-                post = p
-                break
-        if post is None:
-            b.abort(404, "No such post")
-        content = b.request.forms.pop('content').decode("utf8")
-        post.compiler.create_post(post.source_path, content=content, onefile=True, is_page=False, **b.request.forms)
-        init_site()
-        b.redirect('/edit/' + path)
-
-    @staticmethod
-    @b.route('/delete/<path:path>')
-    def delete(path):
-        context = {'path': path}
-        context['site'] = _site
-        post = None
-        for p in _site.timeline:
-            if p.source_path == path:
-                post = p
-                break
-        if post is None:
-            b.abort(404, "No such post")
-        context['post'] = post
-        return render('delete_post.tpl', context)
-
-    @staticmethod
-    @b.route('/really_delete/<path:path>')
-    def really_delete(path):
-        # FIXME insecure pending defnull/bottle#411
-        os.unlink(path)
-        init_site()
-        b.redirect('/')
-
-    @staticmethod
-    @b.route('/static/<path:path>')
-    def server_static(path):
-        return b.static_file(path, root=os.path.join(os.path.dirname(__file__), 'static'))
-
-    @staticmethod
-    @b.route('/new/post', method='POST')
-    def new_post():
-        title = b.request.forms['title']
-        # So, let's create a post with that title, lumberjack style
-        # FIXME but I am a lumberjack and I don't care.
-        os.system("nikola new_post -t '{0}'".format(title))
-        # reload post list and go to index
-        init_site()
-        b.redirect('/')
-
-    @staticmethod
-    @b.route('/new/page', method='POST')
-    def new_page():
-        title = b.request.forms['title']
-        # So, let's create a page with that title, lumberjack style
-        # FIXME but I am a lumberjack and I don't care.
-        os.system("nikola new_page -t '{0}'".format(title))
-        # reload post list and go to index
-        init_site()
-        b.redirect('/')
-
-lookup = mako.lookup.TemplateLookup(
-    directories=os.path.join(os.path.dirname(__file__), 'templates'),
-    output_encoding='utf-8')
-
-
-def render(template_name, context=None):
-    if context is None:
-        context = {}
-    return lookup.get_template(template_name).render_unicode(**context)
+            webbrowser.open('http://coil.readthedocs.org/admin/setup')
