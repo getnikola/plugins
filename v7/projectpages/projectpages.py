f243be19e7c0d021ee5d7b3f26425b448f933302
Chris Warrick <kwpolska@gmail.com>
2016-03-04 20:32:57 +0100

    projectpages: add some classes in projects.tmpl
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- v7/projectpages/projectpages.py
+++ v7/projectpages/projectpages.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright © 2014–2015, Chris Warrick.
+# Copyright © 2014–2016, Chris Warrick.
 
 # Permission is hereby granted, free of charge, to any
 # person obtaining a copy of this software and associated
