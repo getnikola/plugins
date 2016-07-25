d4187c3a8f42e71ad52bc1f7b07e1467d197e9a2
chillaranand <anand21nanda@gmail.com>
2016-07-25 10:15:31 +0530

    Ensure site url ends with leading slash

--- v7/import_blogger/import_blogger.py
+++ v7/import_blogger/import_blogger.py
@@ -115,7 +115,11 @@ class CommandImportBlogger(Command, ImportMixin):
         context['BLOG_TITLE'] = channel.feed.title
 
         context['BLOG_DESCRIPTION'] = ''  # Missing in the dump
-        context['SITE_URL'] = channel.feed.link
+        if not channel.feed.link.endswith('/'):
+            context['SITE_URL'] = channel.feed.link + '/'
+        else:
+            context['SITE_URL'] = channel.feed.link
+
         context['BLOG_EMAIL'] = channel.feed.author_detail.email
         context['BLOG_AUTHOR'] = channel.feed.author_detail.name
         context['POSTS'] = '''(
