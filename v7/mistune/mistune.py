34c3b9af72f40db9e573ddb531a955551015906c
Roberto Alsina <ralsina@netmanagers.com.ar>
2016-09-24 12:17:41 -0300

    Add Mistune compiler

--- v7/mistune/mistune.py
+++ v7/mistune/mistune.py
@@ -24,7 +24,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-"""Implementation of compile_html based on CommonMark."""
+"""Implementation of compile_html based on Mistune."""
 
 from __future__ import unicode_literals
 
@@ -54,10 +54,10 @@ class CompileMistune(PageCompiler):
     def __init__(self, *args, **kwargs):
         super(CompileMistune, self).__init__(*args, **kwargs)
         if mistune is not None:
-            self.parser = Cmistune.Markdown()
+            self.parser = mistune.Markdown()
 
     def compile_html(self, source, dest, is_two_file=True):
-        if CommonMark is None:
+        if mistune is None:
             req_missing(['mistune'], 'build this site (compile with mistune)')
         makedirs(os.path.dirname(dest))
         with codecs.open(dest, "w+", "utf8") as out_file:
