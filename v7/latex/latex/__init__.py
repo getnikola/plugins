d2d02b552eba8fe2e2364693602c6966b0edb944
Felix Fontein <felix@fontein.de>
2017-01-16 21:38:27 +0100

    Fixing description.

--- v7/latex/latex/__init__.py
+++ v7/latex/latex/__init__.py
@@ -24,7 +24,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-"""Compile LaTeX-similar text to HTML."""
+"""Compile a subset of LaTeX to HTML."""
 
 from __future__ import unicode_literals
 
