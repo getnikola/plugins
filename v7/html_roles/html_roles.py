0772feda33d9134ab22a71db0d11df928f35bbf5
Saša Janiška <gour@atmarama.com>
2018-04-20 12:28:03 +0200

    use <s> tag instead of deprecated <strike>

--- v7/html_roles/html_roles.py
+++ v7/html_roles/html_roles.py
@@ -30,7 +30,7 @@ from docutils.parsers.rst import roles
 from nikola.plugin_categories import RestExtension
 
 TAGS = {
-    'strike': 'strike',
+    's': 's',
     'del': 'del',
     'ins': 'ins',
 }
