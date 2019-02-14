a0914bc58cde0d66bd60ba3cb8912229b01353b7
Surya Sankar <suryashankar.m@gmail.com>
2019-02-14 20:32:55 +0530

    fixing indentation issues. Removing unnecessary imports

--- v8/hide_input_cells/hide_input_cells.py
+++ v8/hide_input_cells/hide_input_cells.py
@@ -1,24 +1,20 @@
 from __future__ import unicode_literals
 from nikola.plugin_categories import ConfigPlugin
-from nikola import utils
-from nikola.utils import LOGGER
-
-import re
 
 
 class HideInputCells(ConfigPlugin):
-  def set_site(self, site):
-    site.template_hooks['extra_head'].append(
-      """
-      <!--Code inserted by HideInputCells -->
-      <style type="text/css">
-          div.input {
-              display: none;
-          }
-          div.prompt {
-              display: none;
-          }
-      </style>
-      """
-    )
-    return super(HideInputCells, self).set_site(site)
+    def set_site(self, site):
+        site.template_hooks['extra_head'].append(
+            """
+            <!--Code inserted by HideInputCells -->
+            <style type="text/css">
+              div.input {
+                  display: none;
+              }
+              div.prompt {
+                  display: none;
+              }
+            </style>
+            """
+        )
+        return super(HideInputCells, self).set_site(site)
