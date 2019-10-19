0c312b4d6d13e839c74f1394b6616df9c98ba0d1
th <huhuang03@gmail.com>
2019-10-19 16:56:58 +0800

    add command when org-mode failed

--- v8/orgmode/orgmode.py
+++ v8/orgmode/orgmode.py
@@ -90,7 +90,8 @@ class CompileOrgmode(PageCompiler):
                 req_missing(['emacs', 'org-mode'],
                             'use the orgmode compiler', python=False)
         except subprocess.CalledProcessError as e:
-            raise Exception('Cannot compile {0} -- bad org-mode configuration (return code {1})'.format(source, e.returncode))
+            raise Exception('''Cannot compile {0} -- bad org-mode configuration (return code {1})
+The command: is {2}'''.format(source, e.returncode, " ".join(e.cmd)))
 
     def create_post(self, path, content=None, onefile=False, is_page=False, **kw):
         """Create post file with optional metadata."""
