796f50839b8e6cc9adcb7d6ff87b44ef265a1a5b
x1101 <x1101@gmx.com>
2023-03-09 15:33:06 -0500

    WIP, hax

--- v8/podcast_rss/podcast_rss.py
+++ v8/podcast_rss/podcast_rss.py
@@ -44,10 +44,15 @@ class GeneratePodcastRSS(TaskMultiplier):
     def process(self, task, prefix):
 
         """Process Tasks"""
+        if not self.site.config['PODCAST_RSS']:
+            return []
         if task.get('name') is None:
             return []
 
         utils.LOGGER.warn("process called for Gen Pod Feed")
+        utils.LOGGER.warn(self.site.path_handlers)
+        # utils.LOGGER.warn(self.site.config['TRANSLATIONS'])
+        # utils.LOGGER.warn("Targets: %s", task.get('targets', []))
         kw = {
             "translations": self.site.config["TRANSLATIONS"],
             "filters": self.site.config["FILTERS"],
@@ -62,6 +67,7 @@ class GeneratePodcastRSS(TaskMultiplier):
             "feed_read_more_link": self.site.config["FEED_READ_MORE_LINK"],
             "feed_links_append_query": self.site.config["FEED_LINKS_APPEND_QUERY"],
         }
+        utils.LOGGER.warn("KW Set")
 
         feed_task = {
             'file_dep': [],
@@ -70,15 +76,22 @@ class GeneratePodcastRSS(TaskMultiplier):
             'uptodate': [],
             'basename': '{0}_podcast_rss'.format(prefix),
             'name': task.get('name').split(":", 1)[-1],
-            'task_dep': ['render_posts'],
+            'task_dep': ['render_site'],
             'clean': True
         }
+        utils.LOGGER.warn("Feed Task Set")
 
         deps = []
         deps_uptodate = []
         for feed in self.site.config['PODCAST_FEEDS']:
+            feed_name = "rss_" + feed
+            utils.LOGGER.warn("Adding action for: %s", feed)
             for lang in kw['translations']:
-                output_name = os.path.join(kw['output_folder'], self.site.path('rss_' + feed, None, lang))
+                utils.LOGGER.warn("Doing work for: %s", lang)
+                utils.LOGGER.warn("output folder: %s", kw['output_folder'])
+                utils.LOGGER.warn("output path: %s", self.site.path(feed_name, None, lang))
+                output_name = os.path.join(kw['output_folder'], self.site.path(feed_name, None, lang))
+                utils.LOGGER.warn("Setting Output to: %s", output_name)
 
                 if kw["show_untranslated_posts"]:
                     posts = self.site.posts[:kw['feed_length']]
@@ -114,19 +127,27 @@ class GeneratePodcastRSS(TaskMultiplier):
             mime = mimetypes.guess_type(url)[0]
             return url, length, mime
 
-    def _rss_path(self, lang, feed):
+    """
+    Honestly, does this need to be a method? can I make it some kind of lambda?
+    """
+    def _rss_path(self, name, lang):
         """A link to the RSS feed path.
 
         Example:
 
         link://rss => /blog/rss_ogg.xml
         """
-        return [_f for _f in [self.site.config['TRANSLATIONS'][lang],
-                              self.site.config['RSS_PATH'], 'rss_' + feed + '.xml'] if _f]
+        utils.LOGGER.warn("name: %s", name)
+        res = []
+        for feed in self.site.config['PODCAST_FEEDS']:
+            res.append([_f for _f in [self.site.config['TRANSLATIONS'][lang],
+                                      self.site.config['RSS_PATH'], 
+                                      'rss_' + feed + '.xml'] 
+                        if _f])
+        return res
 
     def set_site(self, site):
         """Set Nikola site."""
         for feed in site.config['PODCAST_FEEDS']:
             site.register_path_handler('rss_' + feed, self._rss_path)
-            utils.LOGGER.warn("registering rss_" + feed)
         return super(GeneratePodcastRSS, self).set_site(site)
