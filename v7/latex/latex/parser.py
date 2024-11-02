getnikola/plugins

commit f7b2e69e133fcf46332cb3cbb18b4dd2daeb79ed

Replace type comparison with isinstance

v7/latex/latex/parser.py
@@ -338,10 +338,10 @@ class Parser:
         if isinstance(block, tree.Block):
             while True:
                 if len(block.elements) == 1 and isinstance(block.elements[0], tree.Block):
-                    if type(block) == tree.Block:
+                    if isinstance(block, tree.Block):
                         block.elements[0].labels.extend(block.labels)
                         block = block.elements[0]
-                    elif type(block.elements[0]) == tree.Block:
+                    elif isinstance(block.elements[0], tree.Block):
                         block.labels.extend(block.elements[0].labels)
                         block.elements = block.elements[0].elements
                     else:
@@ -711,7 +711,7 @@ class Parser:
 
     def __read_formula(self, delimiter):
         start = self.tokens.current_indices()[0]
-        delimitedByEnvironmentEnd = (type(delimiter) == tuple)
+        delimitedByEnvironmentEnd = isinstance(delimiter, tuple)
         while self.tokens.has_current():
             stop = self.tokens.current_indices()[0]
             token = self.tokens.current_type()

v7/latex/latex/__init__.py
@@ -206,7 +206,7 @@ class CompileLaTeX(nikola.plugin_categories.PageCompiler):
         if os.path.isfile(dep_path):
             with io.open(dep_path, 'rb') as file:
                 result = json.loads(file.read().decode('utf-8'))
-            if type(result) == list and len(result) == 4:
+            if isinstance(result, list) and len(result) == 4:
                 return result
         return ([], [], [], [])
 

v7/latex/latex/tree.py
@@ -355,7 +355,7 @@ class PSTricksPicture(Formula):
         """Initialize object."""
         super(PSTricksPicture, self).__init__(formula)
         self.args = args
-        assert type(self.args) == dict
+        assert isinstance(self.args, dict)
 
     def recombine_as_text(self, reescape=True):
         """Recombine subtree as LaTeX source text."""

v7/latex_formula_renderer/latex_formula_renderer.py
@@ -318,7 +318,7 @@ class FormulaCache(object):
         try:
             with open(self.__get_database_file(), "rb") as file:
                 result = json.loads(file.read().decode('utf-8'))
-            if type(result) != list or len(result) != 2 or type(result[0]) != dict or type(result[1]) != list:
+            if not isinstance(result, list) or len(result) != 2 or not isinstance(result[0], dict) or not isinstance(result[1], list):
                 raise Exception("Read database invalid!")
             result[1] = set(result[1])
             return result

v7/wordpress_compiler/wordpress/shortcodes.py
@@ -30,10 +30,10 @@ def shortcode_atts(pairs, atts):
     # * @param array $pairs Entire list of supported attributes and their defaults.
     # * @param array $atts User defined attributes in shortcode tag.
     # * @return array Combined attribute list.
-    if type(atts) == str:
+    if isinstance(atts, str):
         atts = {None: atts}
     out = dict()
-    if type(pairs) == dict:
+    if isinstance(pairs, dict):
         pairs = pairs.items()
     for key, value in pairs:
         if key in atts:

v7/wordpress_compiler/wordpress/wordpress.py
@@ -212,7 +212,7 @@ class CompileWordpress(PageCompiler):
         if os.path.isfile(dep_path):
             with io.open(dep_path, 'rb') as file:
                 result = json.loads(file.read().decode('utf-8'))
-            if type(result) == list and len(result) == 4:
+            if isinstance(result, list) and len(result) == 4:
                 return result
         return ([], [], [], [])
 

commit e1e832acd87fe091960393764839570460ac04b5

Fix bug introduced in f7b2e69e133fcf46332cb3cbb18b4dd2daeb79ed.
Here checking exactly for Block is important; classes derived
from Block must not match.

v7/latex/latex/parser.py
@@ -338,10 +338,12 @@ class Parser:
         if isinstance(block, tree.Block):
             while True:
                 if len(block.elements) == 1 and isinstance(block.elements[0], tree.Block):
-                    if isinstance(block, tree.Block):
+                    # Note that the two if conditions below cannot use isinstance()
+                    # since we do not want to match any class derived from Block!
+                    if type(block) == tree.Block:
                         block.elements[0].labels.extend(block.labels)
                         block = block.elements[0]
-                    elif isinstance(block.elements[0], tree.Block):
+                    elif type(block.elements[0]) == tree.Block:
                         block.labels.extend(block.elements[0].labels)
                         block.elements = block.elements[0].elements
                     else: